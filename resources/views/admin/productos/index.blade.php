12f2f163c17a7fdb723d1a9cf9252f2ba07ec8f0
Kevin <kevin.galaxy04@gmail.com>
2022-10-20 20:00:10 -0300

    partials

--- resources/views/admin/productos/index.blade.php
+++ resources/views/admin/productos/index.blade.php
@@ -7,18 +7,24 @@
 
 @section('content')
     @if (session('info'))
-        <div class="alert alert-info" >
-            <strong>{{session('info')}}</strong>
-        </div>
+    <div class="alert alert-info" role="alert" >
+    <strong>{{session('info')}}</strong>
+    </div>
 
 
     @elseif (session('success'))
-        <div class="alert alert-primary">
+        <div class="alert alert-primary" role="alert">
             <strong>{{session('success')}}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
         </div>
     @elseif (session('danger'))
         <div class="alert alert-danger">
             <strong>{{session('danger')}}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
         </div>
     @endif
     <div class="card">
@@ -59,31 +65,33 @@
                                 <form action="{{route('admin.productos.destroy',$producto)}}" method="POST">
                                     @csrf
                                     @method('delete')
-                                    <input type="submit" onclick="return confirm(¿Quiere Eliminar el Producto?)" class=" text-white btn bg-red-800" value="Eliminar" >
+                                    <button type="submit" onclick="return confirm('¿Quiere Eliminar el Producto?')" class=" text-white btn bg-red-800">Eliminar</button>
+                                    {{-- <div class="modal" tabindex="-1" role="dialog">
+                                        <div class="modal-dialog" role="document">
+                                          <div class="modal-content">
+                                            <div class="modal-header">
+                                              <h5 class="modal-title">Modal title</h5>
+                                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                              </button>
+                                            </div>
+                                            <div class="modal-body">
+                                              <p>Modal body text goes here.</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                              <button type="button" class="btn btn-primary">Save changes</button>
+                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div> --}}
                                 </form>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
-            {{-- <form action="">
-                <div>
-                    <label class="block text-sm font-medium text-gray-700">Cover photo</label>
-                    <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                      <div class="space-y-1 text-center">
-                        <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                          <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                        <div class="flex text-sm text-gray-600">
-                          <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
-                            <span>Upload a file</span>
-                            <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                          </label>
-                          <p class="pl-1">or drag and drop</p>
-                        </div>
-                        <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                      </div>
-            </form> --}}
+
         </div>
 
         {{-- <div class="card-footer">
