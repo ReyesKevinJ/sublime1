999b862c8a7b2fe2036be4c64d9c612932c3376e
Kevin <kevin.galaxy04@gmail.com>
2022-10-29 14:06:58 -0300

    29-10 14:06

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -3,9 +3,21 @@
 namespace App\Actions\Fortify;
 
 use Laravel\Fortify\Rules\Password;
+// Require at least 10 characters...
+(new Password)->length(10);
+
+// Require at least one uppercase character...
+(new Password)->requireUppercase();
+
+// Require at least one numeric character...
+(new Password)->requireNumeric();
+
+// Require at least one special character...
+(new Password)->requireSpecialCharacter();
 
 trait PasswordValidationRules
 {
+
     /**
      * Get the validation rules used to validate passwords.
      *
@@ -13,6 +25,6 @@ trait PasswordValidationRules
      */
     protected function passwordRules()
     {
-        return ['required', 'string', new Password, 'confirmed'];
+        return ['required', 'string', (new Password)->length(8)->requireUppercase()->requireNumeric()->requireSpecialCharacter(), 'confirmed'];
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Admin\ProductoController;
 use App\Http\Controllers\Admin\PedidosController;
 use App\Http\Controllers\Admin\UserController;
 
-Route::get('', [HomeCoWtroller::class, 'index'])->middleware('can:admin.home')->name('admin.home');
+Route::get('', [HomeController::class, 'index'])->middleware('can:admin.home')->name('admin.home');
 Route::resource('productos', ProductoController::class)->names('admin.productos')->middleware('can:admin.home');
 Route::resource('pedidos', PedidosController::class)->names('admin.pedidos')->middleware('can:admin.home');
 Route::resource('users',UserController::class)->only('index','edit','update')->middleware('can:admin.home')->names('admin.users');
