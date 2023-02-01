c33b7d69e40d831fd22488582c3cf7f45fca1ca5
ReyesKevinJ <102432119+ReyesKevinJ@users.noreply.github.com>
2023-02-01 16:39:19 -0300

    1-2 16:39

--- resources/views/livewire/carrito-index.blade.php
+++ resources/views/livewire/carrito-index.blade.php
@@ -4,8 +4,8 @@
       <!-- <div class="flex items-center justify-center py-8">
         <button onclick="checkoutHandler(false)" class="py-2 px-10 rounded bg-indigo-600 hover:bg-indigo-700 text-white">Open Modal</button>
     </div> -->
-    <div class="w-full h-full z-40 bg-opacity-90 top-0 overflow-y-auto overflow-x-hidden fixed sticky-0" id="chec-div">
-        <div class="w-full absolute z-10 right-0 h-full overflow-x-hidden transform translate-x-0 transition ease-in-out duration-700" id="checkout">
+    <div class="w-full h-full z-40 bg-opacity-90 top-0 overflow-y-auto overflow-x-hidden fixed sticky-0 hidden" id="chec-div">
+        <div class="w-full absolute z-10 right-0 h-full overflow-x-hidden transform translate-x-full transition ease-in-out duration-700" id="checkout">
             <div class="flex items-end lg:flex-row flex-col justify-end" id="cart">
                 <div class="lg:w-1/2 md:w-8/12 w-full lg:px-8 lg:py-14 md:px-6 px-4 md:py-8 py-4 bg-white dark:bg-gray-800 overflow-y-auto overflow-x-hidden lg:h-screen h-auto" id="scroll">
                     <div class="flex items-center text-gray-500 hover:text-gray-600 dark:text-white cursor-pointer" onclick="checkoutHandler(false)">
@@ -18,18 +18,18 @@
                     @foreach (Cart::content() as $item)
                     <div class="md:flex items-strech py-8 md:py-10 lg:py-8 border-t border-gray-50">
                         <div class="md:w-4/12 2xl:w-1/4 w-full">
-                             <img src="{{url($productos->image->url)}}" alt="Black Leather Bag" class="h-full object-center object-cover md:block hidden" />
-                             <img src="h{{Storage::url($productos->image->url)}}" alt="Black Leather Bag" class="md:hidden w-full h-full object-center object-cover" />  
+                            <img src="{{url($item->options->urlfoto)}}" alt="Black Leather Bag" class="h-full object-center object-cover md:block hidden" />
+                            
                         </div>
                         <div class="md:pl-3 md:w-8/12 2xl:w-3/4 flex flex-col justify-center">
                             <p class="text-xs leading-3 text-gray-800 dark:text-white md:pt-0 pt-4">{{$item->id}}</p>
                             <div class="flex items-center justify-between w-full pt-1">
                                 <p class="text-base font-black leading-none text-gray-800 dark:text-white">{{$item->name}}</p>
-                                <a href="/incrmentar/{{$item->rowId}}" class="btn btn-success">+</a>
                                 <div class="py-2 px-1 border border-gray-200 mr-6 focus:outline-none dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white">
+                                    <a href="/incrmentar/{{$item->rowId}}" class="btn btn-success">+</a>
                                 {{$item->qty}}
-                                </div>
                                 <a href="/decrementar/{{$item->rowId}}" class="btn btn-success">-</a>
+                                </div>
                             </div>
                            
                             <p class="text-xs leading-3 text-gray-600 dark:text-white py-4">Color: {{$item->options->color}}</p>
@@ -63,7 +63,7 @@
                         <div>
                             <div class="flex items-center pb-6 justify-between lg:pt-5 pt-20">
                                 <p class="text-2xl leading-normal text-gray-800 dark:text-white">Total</p>
-                                <p class="text-2xl font-bold leading-normal text-right text-gray-800 dark:text-white">$10,240</p>
+                                <p class="text-2xl font-bold leading-normal text-right text-gray-800 dark:text-white">{{Cart::subtotal()}}</p>
                             </div>
                       
                         </div>
@@ -79,10 +79,10 @@ let checdiv = document.getElementById("chec-div");
 let flag3 = false;
 const checkoutHandler = () => {
     if (!flag3) {
-        checkout.classList.add("translate-x-full");
-        checkout.classList.remove("translate-x-0");
+        checkout.classList.add("translate-x-0");
+        checkout.classList.remove("translate-x-full");
         setTimeout(function () {
-            checdiv.classList.add("hidden");
+            checdiv.classList.remove("hidden");
         }, 1000);
         flag3 = true;
     } else {
@@ -90,7 +90,7 @@ const checkoutHandler = () => {
             checkout.classList.remove("translate-x-full");
             checkout.classList.add("translate-x-0");
         }, 1000);
-        checdiv.classList.remove("hidden");
+        checdiv.classList.add("hidden");
         flag3 = false;
     }
 };
